c82c8694368eeecde02d6d3e56fff0605b713495
Momuhoka <linyer040312@qq.com>
2024-05-11 22:24:35 +0800

    修复samples

---
+++ samples/GwFPGAspi_st7735lcd_v2/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,17 @@
+//Copyright (C)2014-2023 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.9 Beta-3
+//Part Number: GW1N-LV1QN48C6/I5
+//Device: GW1N-1
+//Created Time: Fri Apr 26 16:39:15 2024
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout_o), //output clkout
+        .clkin(clkin_i) //input clkin
+    );
+
+//--------Copy end-------------------
